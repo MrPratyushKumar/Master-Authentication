204228c4bb986c22ead22e5879106e229b1dd68c
Pratyush Pandey <pratyushkr4148@gmail.com>
2026-03-17 15:12:25 +0530

    we have just done the route setup for the authentication

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,18 @@
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
 import dotenv from "dotenv";
+import autRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
+
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Hello world We are on a Home Page Now!");
 });
+
+// use inbuilt  controller 
+app.use("/api/auth" , autRoutes)
 
 app.listen(3000, () => {
   connectDB();
