24aac0e55eb1c73291270e69010475e8eb1d3fca
Pratyush Pandey <pratyushkr4148@gmail.com>
2026-03-17 14:53:46 +0530

    Have done the connectio database to mongoose successfully and also server is running on the port successfully

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+// const express = require('express'); // common js module
+
+// ES6 module
+import express from "express";
+import { connectDB } from "./db/connectDB.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world We are on a Home Page Now!");
+});
+
+app.listen(3000, () => {
+  connectDB();
+  console.log("Server is running on port 3000");
+});
